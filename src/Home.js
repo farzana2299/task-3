3c99d2adb1acb5c22dcc660b09421cb65a3ab1d9
farzana <farzanaka22@gmail.com>
2023-11-23 22:26:28 +0530

    first update

---
+++ src/Home.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import * as XLSX from "xlsx"
+function Home() {
+    const [data, setData] = useState([])
+
+    const handleFileUpload = (e) => {
+        const reader = new FileReader();
+        reader.readAsBinaryString(e.target.files[0]);
+        reader.onload = (e) => {
+            const data = e.target.result;
+            const workbook = XLSX.read(data, { type: "binary" });
+            const sheetName = workbook.SheetNames[0];
+            const sheet = workbook.Sheets[sheetName];
+            const parsedData = XLSX.utils.sheet_to_json(sheet);
+            setData(parsedData)
+        };
+    }
+    return (
+        <div className='container w-50  mdiv'>
+            <div class="input-group mb-3">
+                <input type="file" accept=".xlsx, .xls" onChange={handleFileUpload}
+                    class="form-control" id="inputGroupFile02" />
+                
+            </div>
+
+            {
+                data?.length > 0 && (
+                    <table className='table table-bordered'>
+                        <thead>
+                            <tr>
+                                {Object.keys(data[0]).map((key) => (
+                                    <th key={key}>{key}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+{
+    data.map((row,index)=>(
+        <tr key={index}>
+            {Object.values(row).map((value,index)=>(
+                <td key={index}>{value}</td>
+            ))}
+        </tr>
+    ))
+}
+                        </tbody>
+                    </table>
+                )
+            }
+        </div>
+    )
+}
+
+export default Home
